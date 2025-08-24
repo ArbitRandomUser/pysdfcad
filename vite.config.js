580f529b28a3938302e486a374bf02c1c506df6e
John George Francis <arbitrandomuser@gmail.com>
2025-08-24 14:41:01 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   //    'Cross-Origin-Opener-Policy': 'same-origin',
   //  },
   //}
-  //base: "/thangs/sdfcsg"
+  base: "/thangs/pysdfcad"
 });
 
 const config = {
